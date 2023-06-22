824f77835c5766b6ca887567e20da5bd12732486
samytaglia <samytaglia@gmail.com>
2023-06-22 20:27:05 -0300

    Stylesheets

--- src/components/pages/home/HomeContainer.jsx
+++ src/components/pages/home/HomeContainer.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import Home from "./Home";
 
-const HomeContainer = ({ nombre }) => {
-  const [userName, setUserName] = useState("pepito");
-  console.log("me renderize");
-
-  return <Home nombre={nombre} userName={userName} setUserName={setUserName} />;
+const HomeContainer = () => {
+  return <Home/>;
 };
 
 export default HomeContainer;
--- src/components/pages/itemList/ItemList.jsx
+++ src/components/pages/itemList/ItemList.jsx
@@ -2,7 +2,7 @@
 const ItemList = () => {
   return (
     <div>
-       Productos
+
     </div>
   )
 }
