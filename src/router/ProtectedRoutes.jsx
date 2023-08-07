a0ac452fbc1ddf06488e7d3392e0ee5fe7d1e858
samytaglia <samytaglia@gmail.com>
2023-08-06 23:49:16 -0300

    Fixing vercel issues

--- src/router/ProtectedRoutes.jsx
+++ src/router/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router";
 
 const ProtectedRoutes = () => {
 
-    let userRol = "admin"
+    let userRol = "customer"
 
   return (
     <div>{userRol === "admin" ? <Outlet/> : <Navigate to="/"/> }</div>
